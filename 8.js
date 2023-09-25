fda9775a2d1437f84c0e2bb0f8d4d95796479492
OliinykOS <52663389+OliinykOS@users.noreply.github.com>
2023-09-25 14:18:21 +0300

    js-train-24 done

--- 8.js
+++ 8.js
@@ -1,12 +1,24 @@
 console.log("Завдання: 8 ==============================");
 
 function task8() {
-  // Створюємо функцію fetchFakeData, яка симулює запит до сервера та повертає "дані" через Promise.resolve().
-  // Створюємо fakeData константу з данними { name: "John",age: 30}
-  // Використовуємо Promise.resolve(fakeData) для того, щоб створити вже зарезолвлений проміс.
-  // Викликаємо функцію fetchFakeData
-  // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
-  // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+    // Створюємо функцію fetchFakeData, яка симулює запит до сервера та повертає "дані" через Promise.resolve().
+    function fetchFakeData(data) {
+        return Promise.resolve(data);
+    }
+    // Створюємо fakeData константу з данними { name: "John",age: 30}
+    const fakeData = { name: "John", age: 30 };
+    // Використовуємо Promise.resolve(fakeData) для того, щоб створити вже зарезолвлений проміс.
+    Promise.resolve(fakeData);
+    // Викликаємо функцію fetchFakeData
+    fetchFakeData(fakeData)
+        // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
+        .then((data) => {
+            console.log(data);
+        })
+        // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 // Викликаємо функцію task8
